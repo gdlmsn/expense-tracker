666d23cd14a1e5268eb61f057df74813ef31b171
Giulio <gdalmasson@gmail.com>
2021-03-20 10:50:23 +1100

    Transactions

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ import { IncomeExpenses } from './components/IncomeExpenses';
 import { TransactionList } from './components/TransactionList';
 import { AddTransaction } from './components/AddTransaction';
 
-
+import {GlobalProvider} from './context/GlobalState'
 function App() {
   return (
-    <div>
+    <GlobalProvider>
       <Header title='Expense Tracker' />
       <div className='container'>
         <Balance/>
@@ -16,7 +16,7 @@ function App() {
         <TransactionList/>
         <AddTransaction/>
       </div>
-    </div>
+    </GlobalProvider>
   );
 }
 
