fa417e4ce7fdb701852fe37b330fd2ee20b2017f
Giulio <gdalmasson@gmail.com>
2021-03-30 10:12:30 +1100

    Server

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express');
 const dotenv = require('dotenv');
 const colors = require('colors');
@@ -15,7 +16,18 @@ const app = express();
 //allows use of body parser
 app.use(express.json());
 
-app.use('/api/v1/transactions', transactions)
+//morgan
+if(process.env.NODE_ENV === 'development'){
+    app.use(morgan('dev'))
+}
+
+app.use('/api/v1/transactions', transactions);
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'));
+
+    app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+}
 
 const PORT = process.env.PORT || 5000;
 
